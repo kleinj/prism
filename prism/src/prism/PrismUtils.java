1a87267f3e2daa42bdc8eaeb5b266781d1b71f7e
Joachim Klein <klein@tcs.inf.tu-dresden.de>
2018-07-24 18:21:42 +0200

    PrismUtils: formatIntFromDouble convenience formatting methodx

--- prism/src/prism/PrismUtils.java
+++ prism/src/prism/PrismUtils.java
@@ -37,6 +37,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import common.iterable.IterableInt;
+import param.BigRational;
 
 /**
  * Various general-purpose utility methods in Java
@@ -517,6 +518,17 @@ public class PrismUtils
 		// by the 0+ part
 		result = result.replaceFirst("(\\.[0-9]*?)0+(e|$)", "$1$2");
 		return result;
+	}
+
+	/**
+	 * Format a double (that is known to be an integer, but not necessarily in the int range)
+	 * to a string.<br>
+	 * Correctly handles values beyond INT_MIN/INT_MAX, as well as -Inf/Inf and NaN.
+	 */
+	public static String formatIntFromDouble(double d)
+	{
+		BigRational v = BigRational.from(d);
+		return v.toString();
 	}
 
 	/**
